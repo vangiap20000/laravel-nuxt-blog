ff614ac8b4e17caa9d874b33802bedd4964b7351
GiapHV <giaphv.nta@gmail.com>
2024-07-17 17:07:37 +0700

    [Update]: update logic task

--- front_end/src/pages/index.vue
+++ front_end/src/pages/index.vue
@@ -4,7 +4,8 @@
 			<div class="min-h-screen flex overflow-x-scroll py-12">
 				<div v-if="pending" class="fixed w-full h-full bg-[#ffffff]/50 top-0 left-0 z-[60]">
 					<div role="status" class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2">
-						<svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
+						<svg aria-hidden="true"
+							class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
 							viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path
 								d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -29,12 +30,13 @@
 							class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
 							@click="openModal(column.title)">
 							<svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+									d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
 							</svg>
 						</button>
 					</div>
-					<draggable :list="column.tasks" :animation="200"  class="min-h-52"
-						ghost-class="ghost-card" group="tasks" @end="moveTask" :statusId="findStatusByName(status, column.title).id">
+					<draggable :list="column.tasks" :animation="200" class="min-h-52" ghost-class="ghost-card"
+						group="tasks" @end="moveTask" :statusId="findStatusByName(status, column.title).id">
 						<task-card v-for="(task) in column.tasks" :key="task.id" :task="task" class="mt-3 cursor-move"
 							@deleteTask="deleteTask" @editTask="editTask" />
 					</draggable>
@@ -56,13 +58,13 @@
 					</UFormGroup>
 
 					<UFormGroup label="Type" name="type">
-						<USelectMenu v-model="formState.type" :options="typeTask" placeholder="Select type" value-attribute="id"
-							option-attribute="name" />
+						<USelectMenu v-model="formState.type" :options="typeTask" placeholder="Select type"
+							value-attribute="id" option-attribute="name" />
 					</UFormGroup>
 
 					<UFormGroup label="Status" name="status">
-						<USelectMenu v-model="formState.status" :options="status" placeholder="Select status" value-attribute="id"
-							option-attribute="name" />
+						<USelectMenu v-model="formState.status" :options="status" placeholder="Select status"
+							value-attribute="id" option-attribute="name" />
 					</UFormGroup>
 
 					<UFormGroup label="Content" name="content">
@@ -87,6 +89,14 @@
 import { VueDraggableNext as draggable } from 'vue-draggable-next';
 import { RegisterValidationSchema } from '../schemas/AddEditTask';
 
+useHead({
+	title: 'Home',
+});
+
+definePageMeta({
+	middleware: ['auth']
+})
+
 const toast = useToast();
 const isOpen = ref(false);
 const pending = ref(false);
@@ -99,7 +109,7 @@ const formData = {
 	status: "",
 	content: "",
 }
-const formState = reactive({ ... formData});
+const formState = reactive({ ...formData });
 
 const openModal = (statusValue: string) => {
 	const statusId = findStatusByName(status.value, statusValue);
@@ -148,14 +158,23 @@ async function getColumns() {
 }
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-	if (pending.value == true || !event.data.status) return; 
+	if (pending.value == true || !event.data.status) return;
 	pending.value = true;
 
-	const { data: responseData, status } = await useFetch('/api/tasks/', {
+	await useApiFetch("sanctum/csrf-cookie");
+	const token = useCookie('XSRF-TOKEN');
+	const headers: any = {
+		Accept: 'application/json',
+		'X-XSRF-TOKEN': token.value,
+	}
+
+	const { status } = await useFetch('/api/tasks/', {
 		method: 'post',
 		body: event.data,
 		server: false,
-	})
+		watch: false,
+		headers,
+	});
 
 	if (status.value == 'success') {
 		toast.add({
@@ -177,12 +196,22 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 }
 
 async function onSubmitEdit(event: FormSubmitEvent<Schema>) {
-	if (pending.value == true || !event.data.status) return; 
+	if (pending.value == true || !event.data.status) return;
 	pending.value = true;
+
+	await useApiFetch("sanctum/csrf-cookie");
+	const token = useCookie('XSRF-TOKEN');
+	const headers: any = {
+		Accept: 'application/json',
+		'X-XSRF-TOKEN': token.value,
+	}
+
 	const { data: responseData, status } = await useFetch(`/api/tasks/${idTaskEdit.value}`, {
 		method: 'put',
 		body: event.data,
-		server: false
+		server: false,
+		watch: false,
+		headers
 	})
 
 	if (status.value == 'success') {
@@ -206,9 +235,18 @@ async function onSubmitEdit(event: FormSubmitEvent<Schema>) {
 }
 
 async function deleteTask(id: number) {
+
+	await useApiFetch("sanctum/csrf-cookie");
+	const token = useCookie('XSRF-TOKEN');
+	const headers: any = {
+		Accept: 'application/json',
+		'X-XSRF-TOKEN': token.value,
+	}
+
 	const { data: responseData, status, error } = await useFetch(`/api/tasks/${id}`, {
 		method: 'delete',
-		server: false
+		server: false,
+		headers
 	});
 
 	if (status.value == 'success') {
@@ -228,20 +266,29 @@ async function deleteTask(id: number) {
 	}
 }
 
-async function moveTask(evt :any) {
-	if (pending.value == true) return; 
+async function moveTask(evt: any) {
+	if (pending.value == true) return;
+
+	await useApiFetch("sanctum/csrf-cookie");
+	const token = useCookie('XSRF-TOKEN');
+	const headers: any = {
+		Accept: 'application/json',
+		'X-XSRF-TOKEN': token.value,
+	}
+
 	const data = {
 		id: evt.item.getAttribute('idTask'),
-		from_status: evt.from.getAttribute('statusId'), 
+		from_status: evt.from.getAttribute('statusId'),
 		to_status: evt.to.getAttribute('statusId'),
 		new_order: evt.newIndex + 1,
 		old_order: evt.oldIndex + 1
 	}
 
-	const { data: responseData, status} = await useFetch('/api/tasks/move', {
+	const { data: responseData, status } = await useFetch('/api/tasks/move', {
 		method: 'post',
 		body: data,
-		server: false
+		server: false,
+		headers
 	});
 
 	if (status.value == 'success') {
